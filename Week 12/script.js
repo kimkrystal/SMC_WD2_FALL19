61595f4c220ed1e9bfcddc4cfd4c9ab191a720f6
Krystal <kim_krystal@student.smc.edu>
2019-11-13 09:36:28 -0800

    Week 12 In class
    More animations via jQuery

---
+++ Week 12/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('input, label').focus(function(){
+        if($(this).val() =='' ){
+         $('main section.notification-area').append('<p class="notification">Please fill '+$(this).attr('id')+' out!</p>');
+        }
+    });
+       
+    $('input, label').blur(function(){
+        $('p.notification').remove()
+    })
+
+    $('select#FX').change(function(){
+        var currentSelection = $(this).val();
+
+        if(currentSelection === 'show'){
+            $('.box').show(1000);
+        }
+        if(currentSelection === 'hide'){
+            $('.box').hide(1000);
+        }
+        if(currentSelection === 'slidedown'){
+            $('.box').slideDown(1000);
+        }
+        if(currentSelection === 'slideup'){
+            $('.box').slideUp(1000);
+        }
+    })
+});
