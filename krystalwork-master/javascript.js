534e2b1c6d8fa9114da8e62fdb367d6424a61ff8
Krystal <kim_krystal@student.smc.edu>
2019-12-08 10:40:38 -0800

    Final progress

---
+++ krystalwork-master/javascript.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $('.toggle').click(function(){
+        $('.navbar').toggleClass('open');
+        $('main, #mobileHeader').toggleClass('slide-right')
+        $('.toggle').toggleClass('active');
+    });
+
+    $('#donate').click(function(){
+        $('.blur-window').fadeIn('300');
+        $('.close').click(function(){
+            $('.blur-window').fadeOut('300');
+        });
+    });
+});
